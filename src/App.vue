51a8e0b41b7a70372438dd30f5d7b24f384f6784
Stephani Dietz <steph@waya.ai>
2021-11-09 15:27:53 -0600

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+<div class="container">
+<img src="./assets/logo.png" alt="">
+<h1>Comparing JS Frameworks: This one is Svelte</h1>
+<div class="count">{{ count }}</div>
+<button @click="increment()">Increment</button>
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 15px;
+  }
+  img {
+    width: 20%;
+  }
+  h1 {
+    font-size: 25px;
+    font-weight: bold;
+    margin-top: 15px;
+    text-align: center;
+  }
+  .count {
+    font-size: 45px;
+    font-weight: bold;
+    color: green;
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
 </style>
